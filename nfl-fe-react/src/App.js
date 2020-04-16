c9fd6b37b53c6aa0612069536c5c992d5c6246e5
silvodesigns <kevsilvo@gmail.com>
2020-04-16 18:21:00 -0400

    getting json data

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
   const [players, setPlayers] = useState("");
+  useEffect(()=> {
+    fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1')
+      .then(response => response.json())
+      .then(response => setPlayers(response.data))
+      .catch(err =>{console.log("there is an error fetching the data")})
+  },[players])
+   
   return (
     <div className="App">
     </div>
