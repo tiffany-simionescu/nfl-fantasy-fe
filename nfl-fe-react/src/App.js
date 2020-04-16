46e6ca49d3c45b9822fd1ba96505d429f0ac0066
silvodesigns <kevsilvo@gmail.com>
2020-04-16 18:31:37 -0400

    displaying JSON data on HTML

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -5,14 +5,28 @@ function App() {
 
   const [players, setPlayers] = useState("");
   useEffect(()=> {
-    fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1')
-      .then(response => response.json())
-      .then(response => setPlayers(response.data))
-      .catch(err =>{console.log("there is an error fetching the data")})
-  },[players])
+
+    async function fetchAPI() {
+
+    const data = await fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1');
+          data
+            .json()
+            .then(res => setPlayers(res.data))
+            .catch(err => console.log(err))
+  }
+
+
+  fetchAPI()
+
+  })
+
    
   return (
     <div className="App">
+      <div>
+          {JSON.stringify(players[0])}
+          {JSON.stringify(players[1])}
+      </div>
     </div>
   );
 }
