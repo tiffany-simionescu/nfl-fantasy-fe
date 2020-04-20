59eb4f3d4f89f09c89669dba86f053f51c09c768
kevin silvestre <silvo@kevins-MacBook-Pro.local>
2020-04-20 04:27:54 -0400

    separarted results in another file

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,32 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
+import Results from './results/results.js';
+
 function App() {
-
-  const [players, setPlayers] = useState("");
-  useEffect(()=> {
-
-    async function fetchAPI() {
-
-    const data = await fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1');
-          data
-            .json()
-            .then(res => setPlayers(res.data))
-            .catch(err => console.log(err))
-  }
-
-
-  fetchAPI()
-
-  })
-
-   
   return (
     <div className="App">
-      <div>
-          {JSON.stringify(players[0])}
-          {JSON.stringify(players[1])}
-      </div>
+        <Results/>
     </div>
   );
 }
---
+++ nfl-fe-react/src/results/results.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+
+function Results() {
+
+  const [players, setPlayers] = useState("");
+  useEffect(()=> {
+
+    async function fetchAPI() {
+
+    const data = await fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1');
+          data
+            .json()
+            .then(res => setPlayers(res.data))
+            .catch(err => console.log(err))
+  }
+
+  fetchAPI()
+
+  })
+
+   
+  return (
+    <div className="App">
+      <div>
+          {JSON.stringify(players[0])}
+          {JSON.stringify(players[1])}
+      </div>
+    </div>
+  );
+}
+
+export default Results;
