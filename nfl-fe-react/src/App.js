2f96ee6324fcbcb554c499a5675f8ae30e520bb2
Laura <theimer.laura@gmail.com>
2020-04-21 22:41:46 -0400

    moved state to parent, can see the type field now. breaking it down, have mock data in now, but it's not fully working.

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,6 +1,94 @@
-import React from 'react';
+import React, {useEffect, useState, useRef } from "react";
+import styled from "styled-components";
 import './App.css';
-import autocomplete from "./components/autocomplete.js"; 
+// import autocomplete from "./components/autocomplete.js"; 
+
+
+const Autocomplete = () => {
+    const [display, setDisplay] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [search, setSearch] = useState("");
+    const wrapperRef = useRef(null); 
+
+    useEffect(() => {
+        const player = [];
+        const promises = new Array(5)   // this says how long the list is 
+            .fill()  // need to study 
+            .map((value, index) => 
+            fetch(`https://api.github.com/users/lauralyeinc?tab=following`));
+        Promise.all(promises).then(playersArray => {
+            return playersArray.map(value => 
+                value
+                .json()
+                .then(({name }) => 
+                player.push({name})
+            )
+        );
+    });
+    setOptions(player);
+}, []);
+
+    useEffect(() => {
+        window.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            window.removeEventListener("mpousedown", handleClickOutside);
+        };
+    });
+
+    const handleClickOutside = event => {
+        const { current: wrap } = wrapperRef;
+        if (wrap && !wrap.contains(event.target)) {
+            setDisplay(false);
+        }
+    };
+
+    const updatePlayer = player => {
+        setSearch(player);
+        setDisplay(false); 
+    };
+
+    return (
+        <div ref={wrapperRef} className="PlayerAutoSearch">
+            <h1> Player A Search Box </h1>
+            <Input 
+                id="autocomplete" 
+                onClick={() => setDisplay(!display)}
+                value={search}
+                onChange={event => setSearch(event.target.value)}
+                placeholder="Player A"
+            />
+            {display && ( 
+                <div className="autoContainer">
+                    {options
+                    .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
+                    .map((value, index ) => {
+                        return (
+                            <div 
+                                onClick={() => updatePlayer(value.name)}
+                                className="option"
+                                key={index}
+                                tabIndex="0"
+                            >
+                                <span> {value.name} </span>
+                                </div>
+                        );
+                    })}
+                    </div>
+            )}
+        </div>
+    );
+
+};
+
+
+
+
+/* styling */ 
+const Input = styled.input`
+border: 2px solid black; 
+color:  palevioletred;
+background:papayawhip;
+`
 
 function App() {
   return (
@@ -10,7 +98,7 @@ function App() {
     </div>
 
     <div className="Main-Area">
-    <autocomplete/> 
+    <Autocomplete/> 
     </div>
 
     <div className="Foot-Area">
