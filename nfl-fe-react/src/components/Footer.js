a32c85b2adf0fb82b30cd4eb0d698686c780154d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-04-24 13:41:48 -0400

    Finished the Links for the Nav and Footer

--- nfl-fe-react/src/components/Footer.js
+++ nfl-fe-react/src/components/Footer.js
@@ -1,10 +1,20 @@
+// The Links are for the component, AppRouter that Thatcher made
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Footer () {
   return (
-    <div>
-      
+    <div className="footer">
+      <Link to={'/home'} classname="footer-item">
+        Home
+      </Link>
+      <Link to={"/how-to-play"} classname="footer-item">
+        How To Play
+      </Link>
+      <div classname="copyright">
+          Copyright © 2020 Tackle My Trade. All Rights Reserved
+      </div>
     </div>
   )
 }
--- nfl-fe-react/src/components/Nav.js
+++ nfl-fe-react/src/components/Nav.js
@@ -1,10 +1,16 @@
+// The Links are for the component, AppRouter that Thatcher made
 import React from 'react';
-import '../App.css';
+import { Link } from 'react-router-dom';
 
 function Nav () {
   return (
-    <div>
-      
+    <div className="nav">
+      <Link to={'/home'} classname="footer-item">
+        Home
+      </Link>
+      <Link to={"/how-to-play"} classname="footer-item">
+        How To Play
+      </Link>
     </div>
   )
 }
