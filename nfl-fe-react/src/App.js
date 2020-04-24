4de347dfd8bac71d1bff4f31d1d8f2c9efc8faca
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-04-23 23:12:09 -0400

    Nav & Footer files created. Added to App.js

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
 import Results from './results/results.js';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
         <Results/>
-
+      <Footer />
     </div>
   );
 }
---
+++ nfl-fe-react/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Nav () {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Nav;
