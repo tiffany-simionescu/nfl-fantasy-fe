1dea56a1fa8b9aedab9a56aaac1de02910d65dc2
Th5tch3r <amber.phan27@gmail.com>
2020-04-27 00:05:12 -0400

    got the hard part of connecting to flask app and dynamic api from input form done

--- nfl-fe-react/src/results-page/results-page.js
+++ nfl-fe-react/src/results-page/results-page.js
@@ -1,37 +1,94 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Form } from 'semantic-ui-react';
 
-function Results() {
+import ResultsCard from './results-card.js';
 
-  const [players, setPlayers] = useState("");
-  useEffect(()=> {
-
-    async function fetchAPI() {
-
-    const data = await fetch('https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=RD-0150&player1_id=DC-0725&week=1');
-          data
-            .json()
-            .then(res => setPlayers(res.data))
-            .catch(err => console.log(err))
-  }
-
-  fetchAPI();
-
-
-  })
-
-   
-  return (
-    <div className="Results">
-      <div>
-          <div className="player1">
-              {players.length > 1 ? players[0].first : ''}
-
-          </div>
-          {/* {JSON.stringify(players[0])}
-          {JSON.stringify(players[1])} */}
-      </div>
-    </div>
-  );
+const initial_state = {
+  player0_id: "",
+  player1_id: "",
+  week: "",
+  searchResults: [],
 }
 
-export default Results;
+const SearchPlayersStatForm = ({ onFormSubmit = () => {} }) => {
+    const [state, setState] = useState(initial_state);
+    
+    const handleSubmit = async event => {
+      event.preventDefault();
+      const { player0_id, player1_id, week} = state;
+      onFormSubmit(state);
+      const searchResults = await searchNflPLayers({ player0_id, player1_id, week });
+      setState({ ...state, searchResults });
+      console.log(searchResults)
+    };
+
+  const handleChange = event => {
+    setState({
+      ...state, 
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const searchNflPLayers = async ({ player0_id, player1_id, week }) => {
+    try {
+      const { data: { data: searchResults } = {} } = await axios.get(
+        `https://nfl-perfect-trader.herokuapp.com/api/trade/?player0_id=${player0_id}&player1_id=${player1_id}&week=${week}`
+      );
+      return searchResults;
+    } catch (err) {
+      console.log(err)
+    }
+    return [];
+  }
+
+
+  const { player0_id, player1_id, week } = state;
+
+  const PlayersStat = state.searchResults.map(result => {
+    return <ResultsCard 
+      playersname={result.first}
+      // player1name={result.first}
+    />
+  });
+  
+  return(
+    <React.Fragment>
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Input 
+            type="text"
+            name="player0_id"
+            value={player0_id}
+            onChange={handleChange}
+            placeholder="1st player"
+          />
+          <Form.Input 
+            type="text"
+            name="player1_id"
+            value={player1_id}
+            onChange={handleChange}
+            placeholder="2nd player"
+          />
+          <Form.Input 
+            type="number"
+            name="week"
+            value={week}
+            onChange={handleChange}
+            placeholder="week number"
+          />
+          <Form.Button type="submit">Submit</Form.Button>
+        </Form>
+      </div>
+
+      {PlayersStat}
+
+    </React.Fragment>
+  );
+};
+
+export default SearchPlayersStatForm;
+
+
+
