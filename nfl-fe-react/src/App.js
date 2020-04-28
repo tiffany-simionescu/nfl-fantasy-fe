876c51b0832e7ffdca530473727c5fac0faecc55
Laura <theimer.laura@gmail.com>
2020-04-27 22:09:01 -0400

    saving

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,35 +1,35 @@
 import React, {useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import express from "express"; 
-// import axios from "axios"; 
+import axios from "axios"; 
 import './App.css';
-// import autocomplete from "./components/autocomplete.js"; 
+import autocomplete from "./components/autocomplete.js"; 
 
-//dependencies 
-// const { createProxyMiddle } = require('http-proxy-middleware'); 
+dependencies 
+const { createProxyMiddle } = require('http-proxy-middleware'); 
 
 
-// // proxy middleware options
-// const apioptions = {
-//     target: `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb/api/players/`,
-//     changeOrigin: true,
-//     ws: true, //proxy websockets
-//     pathRewrite: {
-//         '^/api/old-path' : '/pai/new-path',  //rewrite path
-//         '^/api/remove/path' : '/path'  // remove base path
-//     },
-//     router: {
-//         // when request.headers.host == 'dev.localhost:3000',
-//         // overide target to localhost:8080
-//         'dev.localhost:3000' : 'http://localhost:8080'
-//     }
-// }
-// //create proxy (without context)
-// const Proxy = createProxyMiddle(apioptions);
-// //mount Proxy in web server
-// const app = express();
-// app.use('/api', Proxy);
-// app.listen(3000); 
+// proxy middleware options
+const apioptions = {
+    target: `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb.json`,
+    changeOrigin: true,
+    ws: true, //proxy websockets
+    pathRewrite: {
+        '^/api/old-path' : '/pai/new-path',  //rewrite path
+        '^/api/remove/path' : '/path'  // remove base path
+    },
+    router: {
+        // when request.headers.host == 'dev.localhost:3000',
+        // overide target to localhost:8080
+        'dev.localhost:3000' : 'http://localhost:8080'
+    }
+}
+//create proxy (without context)
+const Proxy = createProxyMiddle(apioptions);
+//mount Proxy in web server
+const app = express();
+app.use('/api', Proxy);
+app.listen(3000); 
 
 
 const AutocompleteA = () => {
@@ -40,12 +40,12 @@ const AutocompleteA = () => {
 
     useEffect(() => {
         const proxyurl = 'http://cors-anywhere.herokuapp.com/'; // this cor's link is causing problems, 429 TOO MANY REQUESTS. BUT getting a "response" 600 times in console.log ine 24. 
-        const url = `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb/api/players/`; 
+        const url = `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb.json`; 
         const player = [];
         const promises = new Array(600)   // have 600 in the databases  
             .fill()  // need to study
             .map((value, index) => 
-            fetch(proxyurl + url));       // need to see and find the res.data and the name it's call in the API.   
+            axios.fetch(proxyurl + url));       // need to see and find the res.data and the name it's call in the API.   
         Promise.all(promises)
         .then(playerArray => {
             console.log(playerArray); 
@@ -127,7 +127,7 @@ const AutocompleteB = () => {
 
     useEffect(() => {
         const proxyurl = 'http://cors-anywhere.herokuapp.com/'; // this cor's link is causing problems, 429 TOO MANY REQUESTS. BUT getting a "response" 600 times in console.log ine 24. 
-        const url = `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb/api/players/`; 
+        const url = `https://data.heroku.com/dataclips/rwhkccnmdgnbgemujsjivyshywlb.json`; 
         const player = [];
         const promises = new Array(600)   // have 600 in the databases  
             .fill()  // need to study
@@ -135,14 +135,13 @@ const AutocompleteB = () => {
             fetch(proxyurl + url));
         Promise.all(promises)
         .then(playerArray => {
-            console.log(playerArray); 
-            return playerArray;   // errors stop with this set of code for now, // but need below to have options to pick from.
-        //     return playerArray.map(value =>   
-        //         value
-        //         .json()
-        //         .then(({ name }) => 
-        //         player.push({name})
-        //     )
+            console.log(playerArray);   
+            // return playerArray.map(value =>   
+            //     value
+            //     .json()
+            //     .then(({ name }) => 
+            //     player.push({name})
+            // )
         // );
     });
     setOptions(player);
@@ -247,7 +246,7 @@ function App() {
             </div>
 
             <div className="Main-Area">
-            <AutocompleteA/>
+            <AutocompleteA/>  
             <AutocompleteB/>  
             </div>
 
