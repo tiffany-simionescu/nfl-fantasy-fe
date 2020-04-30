bf5564590c0b34cfd0858bd02a1ef12ed7b2a677
Laura <theimer.laura@gmail.com>
2020-04-29 22:06:19 -0400

    saving, to begin work, pulling again also.

---
+++ nfl-fe-react/src/components/DropDownA.js
@@ -0,0 +1,32 @@
+import React from "react"; 
+import styled from "styled-components"; 
+import Dropdown from "react-bootstrap/Dropdown"; 
+
+function DropDownA() { 
+
+    return(
+    <PlayeraArea className="PlayA-Area">
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="player-A">
+                Pick Player A 
+            </Dropdown.Toggle>
+        <Dropdown.Menu className="playerA-dropdown">
+            <Dropdown.Item href="#"> Tom Brady </Dropdown.Item> 
+            <Dropdown.Item href="#"> Aaron Donald </Dropdown.Item>
+            <Dropdown.Item href="#"> Drew Brees </Dropdown.Item>
+            <Dropdown.Item href="#"> Khalil Mack </Dropdown.Item>
+        </Dropdown.Menu>          
+        </Dropdown>
+    </PlayeraArea> 
+    ); 
+}
+
+
+const PlayeraArea = styled.div`
+padding: 5rem; 
+`
+
+export default DropDownA; 
+
+
+
