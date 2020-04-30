e07baf8d726df93514250fe8cbac996389855de3
doanmade <43007082+doanmade@users.noreply.github.com>
2020-04-29 22:01:40 -0500

    working on styling of dropdowns

--- nfl-fe-react/src/components/DropDownB.js
+++ nfl-fe-react/src/components/DropDownB.js
@@ -1,31 +1,27 @@
-import React from "react"; 
-import styled from "styled-components"; 
-import Dropdown from "react-bootstrap/Dropdown"; 
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styled from "styled-components";
 
-function DropDownA() { 
-
-    return(
-    <PlayerbArea className="PlayA-Area">
-    <Dropdown>
+function DropDownA() {
+  return (
+    <PlayerAreaB className="PlayA-Area">
+      <Dropdown>
         <Dropdown.Toggle variant="success" id="player-B">
-            Pick Player B 
-    </Dropdown.Toggle>
-    <Dropdown.Menu className="playerB-dropdown">
-    <Dropdown.Item href="#"> Tom Brady </Dropdown.Item>
-    <Dropdown.Item href="#"> Aaron Donald </Dropdown.Item>
-    <Dropdown.Item href="#"> Drew Brees </Dropdown.Item>
-    <Dropdown.Item href="#"> Khalil Mack </Dropdown.Item>
-    </Dropdown.Menu>          
-    </Dropdown>
-    </PlayerbArea> 
-    ); 
+          Pick Player B
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="playerB-dropdown">
+          <Dropdown.Item href="#"> Tom Brady </Dropdown.Item>
+          <Dropdown.Item href="#"> Aaron Donald </Dropdown.Item>
+          <Dropdown.Item href="#"> Drew Brees </Dropdown.Item>
+          <Dropdown.Item href="#"> Khalil Mack </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </PlayerAreaB>
+  );
 }
 
+const PlayerAreaB = styled.div`
+  padding: 5rem;
+`;
 
-
-
-const PlayerbArea = styled.div`
-padding: 5rem; 
-`
-
-export default DropDownA; 
+export default DropDownA;
