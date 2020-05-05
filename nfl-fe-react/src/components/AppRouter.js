ff7165e30851ca54caca2ee8555935707a7d555e
Th5tch3r <amber.phan27@gmail.com>
2020-05-05 14:08:06 -0400

    adding loser/winner for result card, created searchbar in different route to try to see how can link up player name and id

--- nfl-fe-react/src/components/AppRouter.js
+++ nfl-fe-react/src/components/AppRouter.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ResultsPage from '../results-page/results-page.js';
 import DropDownA from './DropDownA.js';
 import DropDownB from './DropDownB.js'; 
+import Search from './SearchBar.js';
 
 
 export default function AppRouter() {
     return (
         <Router>
-            <Route path="/results" component={ResultsPage} />
             <Route exact path="/" component={DropDownA} /> 
             <Route exact path="/" component={DropDownB} />
+            <Route path="/results" component={ResultsPage} />
+            {/* <Route path="/search" component={Search} /> */}
         </Router>
     )
 }
