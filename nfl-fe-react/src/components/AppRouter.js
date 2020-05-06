67c5078c8f3120d8097eed4e8f887472ce106cff
Laura <theimer.laura@gmail.com>
2020-05-05 21:20:13 -0400

    saving to pull from another branch to get updated content.

--- nfl-fe-react/src/components/AppRouter.js
+++ nfl-fe-react/src/components/AppRouter.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ResultsPage from '../results-page/results-page.js';
-import DropDownA from './DropDownA.js';
-import DropDownB from './DropDownB.js'; 
 
 export default function AppRouter() {
     return (
         <Router>
             <Route path="/results" component={ResultsPage} />
-            <Route exact path="/" component={DropDownA} /> 
-            <Route exact path="/" component={DropDownB} />
         </Router>
     )
 }
