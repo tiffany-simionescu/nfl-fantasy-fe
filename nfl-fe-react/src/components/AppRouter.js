7d1b4e5fa2a79ef304bc244f647fa300c7ac54b7
doanmade <43007082+doanmade@users.noreply.github.com>
2020-05-05 21:14:20 -0500

    search dropdown

--- nfl-fe-react/src/components/AppRouter.js
+++ nfl-fe-react/src/components/AppRouter.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ResultsPage from '../results-page/results-page.js';
-import Search from './SearchBar.js';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ResultsPage from "../results-page/results-page.js";
+// import DropDownA from "./DropDownA.js";
+// import DropDownB from "./DropDownB.js";
+import DropDownForms from "./DropDownForms.js";
+// import Search from "./SearchBar.js";
 
 export default function AppRouter() {
-    return (
-        <Router>
-            <Route path="/results" component={ResultsPage} />
-            <Route path="/results" component={ResultsPage} />
-            {/* <Route path="/search" component={Search} /> */}
-        </Router>
-    )
+  return (
+    <Router>
+      <Route path="/results" component={ResultsPage} />
+      {/* <Route path="/search" component={Search} /> */}
+
+      {/* <Route exact path="/" component={DropDownA} />
+      <hr />
+      <Route exact path="/" component={DropDownB} />
+      <hr /> */}
+      <Route exact path="/" component={DropDownForms} />
+    </Router>
+  );
 }
