21a5fb0da7f1f691c59c3c6aeec5c1e09accfda9
Laura <theimer.laura@gmail.com>
2020-05-06 21:28:55 -0400

    merge conflicts, trying to save to add stashed work into correct branch.

--- nfl-fe-react/src/App.js
+++ nfl-fe-react/src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import Footer from "./components/Footer.js";
+import Nav from "./components/Nav.js";
 
-import Results from './results/results.js';
+
+// Google Analytics for the whole app
+    ReactGA.initialize('UA-145773395-3');
+    const history = createBrowserHistory();
+    // Initialize google analytics page view tracking
+    history.listen(location => {
+        ReactGA.set({ page: location.pathname }); // Update the user's current page
+        ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+    history.listen(location => ReactGA.pageview(location.pathname));
+
 
 function App() {
-  return (
-    <div className="App">
-        <Results/>
+    return (
+        <div className="App">
+            <div className="Nav-Area">
+                <Nav />
+            </div>
 
-    </div>
-  );
-}
+            <div className="Main-Area">
+            <AppRouter/>
+            </div>
+
+            <div className="Foot-Area">
+            </div>
+                <Footer />
+            </div>
+    );
+};
+
 
 export default App;
 
--- nfl-fe-react/src/components/Footer.js
+++ nfl-fe-react/src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer () {
   return (
     <div className="footer">
-      <a href="" classname="footer-item">
+      <a href="#" classname="footer-item">
         Home
       </a>
-      <a href="" classname="footer-item">
+      <a href="#" classname="footer-item">
         How To Play
       </a>
       <div classname="copyright">
