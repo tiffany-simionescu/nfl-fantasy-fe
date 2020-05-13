a501b2ffbb3153b8e44161e85b47a693466206af
Laura <theimer.laura@gmail.com>
2020-05-12 23:35:37 -0400

    styled nav, zero of the jpgs will work. setting it up wrong to have broken images on local host.

--- nfl-fe-react/src/components/Nav.js
+++ nfl-fe-react/src/components/Nav.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import tacklemytradelogo from "../styling/tacklemytradelogo.jpg"; 
+import styled from "styled-components"; 
+import tacklemytradelogo from "../styling/images/tacklemytradelogo.jpg"; 
+
 
 function Nav () {
   return (
-    <div className="nav">
-      <div className="Logo-Area"> <h1> Tackle My Trade</h1> 
-      <img style={{ width: "200px", width: "70px" }} src={{tacklemytradelogo}} alt="tacklemytrade.com" />
-      <a href="#" className="footer-item">
-        Home
-      </a>
-      <a href="#" className="footer-item">
-        How To Play
-      </a>
-    </div>
-    </div> 
+    <NavSection className="nav">
+      <Logo className="Logo-Area"> 
+        <img src={{tacklemytradelogo}} alt="tacklemytrade.com" /> 
+        </Logo>
+        <Anchors className="Link-Area" > 
+          <Testanchor href="https://www.tacklemytrade.com"> Home </Testanchor>
+          <Testanchor href="#"> About Us</Testanchor>
+          <Testanchor href="#" > How To Play </Testanchor>
+        </Anchors> 
+    </NavSection> 
   )
 }
 
 export default Nav;
+
+const NavSection = styled.div`
+padding: 50px; 
+margin: auto 10px; 
+weight: 98%; 
+height: 9rem; 
+// display: inline-block; 
+background: #332e3c; 
+color: #eeeeff
+`
+
+const Logo = styled.div`
+display: inline; 
+margin: 0 auto; 
+`
+const Anchors = styled.div`
+display: inline-flex;
+` 
+const Testanchor = styled.a`
+padding: 1rem;
+font-size: 1.5rem; 
+margin: auto;
+color: #eeeeff; 
+`
