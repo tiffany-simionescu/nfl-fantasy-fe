5f720d73cd1fe40241fef26a373926e54e83626f
Laura <theimer.laura@gmail.com>
2020-05-14 22:16:26 -0400

    fixed logo img size, have the anchors hover set up.

--- nfl-fe-react/src/components/Nav.js
+++ nfl-fe-react/src/components/Nav.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styled from "styled-components"; 
 import tacklemytradelogo from "../styling/images/tacklemytradelogo.jpg"; 
-// this is a message
 
 
 function Nav () {
   return (
     <NavSection className="nav">
-      <Logo className="Logo-Area"> 
-        <img src={tacklemytradelogo} alt="tacklemytrade.com" /> 
-        </Logo>
+      <Logodiv className="Logo-Area"> 
+        <img src={tacklemytradelogo} height="150px"  alt="tacklemytrade.com" /> 
+        </Logodiv>
         <Anchors className="Link-Area" > 
           <Testanchor href="https://www.tacklemytrade.com"> Home </Testanchor>
           <Testanchor href="#"> About Us</Testanchor>
@@ -22,25 +21,29 @@ function Nav () {
 export default Nav;
 
 const NavSection = styled.div`
-padding: 50px; 
-margin: auto 10px; 
-weight: 98%; 
-height: 9rem; 
-// display: inline-block; 
-background: #332e3c; 
-color: #eeeeff
+border: 2px soild red; 
+display: inline-flex; 
+background: #007090;
+width: 100%; 
 `
 
-const Logo = styled.div`
-display: inline; 
-margin: 0 auto; 
+const Logodiv = styled.div`
+border: 2px solid red; 
 `
+
 const Anchors = styled.div`
-display: inline-flex;
-` 
-const Testanchor = styled.a`
-padding: 1rem;
+border: 2px solid red; 
 font-size: 1.5rem; 
-margin: auto;
-color: #eeeeff; 
+display: flex; 
+align-items: center; 
+` 
+
+const Testanchor = styled.a`
+border: 2px solid red; 
+color:#eeeeff;
+padding: 3rem;
+cursor: pointer;  
+&:hover {
+  color: #ffa630;
+  }
 `
