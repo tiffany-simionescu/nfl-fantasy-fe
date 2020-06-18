46686ac65c200303642bf8683b94fd6270708f2a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-06-18 12:26:52 -0400

    Dashboard works when hardcoded.

--- nfl-fe-react/src/components/user/Dashboard.js
+++ nfl-fe-react/src/components/user/Dashboard.js
@@ -1,55 +1,67 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'; 
 import DashboardCard from "./DashboardCard.js"; 
-import { connect } from 'react-redux';
 import { fetchFan } from '../../actions/fan-actions';
+import { connect } from "react-redux";
 
 const Dashboard = (props) => {
 
-    // const [ fan, setFan] = useState([]); // getting all fans from database  
+    const [ fan, setFan] = useState({
+        fan_id: "",
+        first_name: "",
+        last_name: "",
+        city: "",
+        state: "",
+        email: ""
+    }); // getting all fans from database  
 
-    // const all = `https://tacklemytrade-api.herokuapp.com/api/fans`; // normal  
+    const all = `https://tacklemytrade-api.herokuapp.com/api/fans`; // normal  
     // fans/1 does work 
 
-    useEffect(() => {
-        if (!props.loggedIn) {
-          props.fetchFan(props.fan.fan_id);
-          console.log(props.fan)
-        }
-      }, [props.fan]);
-
-    // useEffect (() => {
-    //     axios 
-    //     .get(all)
-    //     .then(res => {
-    //         setFan(res.data);
-    //         res.data.map(fan => console.log(fan))
-    //         console.log(res);
-    //         })
-    //         .catch(err => {
-    //         console.error(err);
-    //         })
-    //     }, []);  
+    useEffect (() => {
+        axios 
+        .get(all)
+        .then(res => {
+            setFan(res.data.filter(fan => fan.fan_id === 1));
+            console.log(fan);
+            })
+            .catch(err => {
+            console.error(err);
+            })
+        }, []);  
 
     return (
-        <div className="Fan-Dashboard">
-            {props.fan ? (
-                <DashboardCard key={props.fan.fan_id} fan={props.fan} />
-            ) : (
-                <p>Loading...</p>
-            )
-                // (fan.map(fan => 
-                // (users.filter(users => users.fan_id === `${userId.fan_id}` => 
-            //         { 
-            //     return(
-            //         <DashboardCard key={fan.fan_id} fan={fan} />
-            //     )
-            // })) : (
-            //     <h4> Is loading ...</h4>
-            // ) 
-        }
+        //  <div className="Fan-Dashboard">
+        //     {props.fans.filter(fan => fan.isLoggedIn === true)  ? 
+        //         (props.fans.filter(fan => fan.isLoggedIn === true).map((fan, index) => 
+        //             // (props.fan.filter(fan => props.fan.includes(fan.fan_id)).map(fan => 
+        //             { 
+        //         return(            
+        //             <DashboardCard key={index} fan={fan} />
+        //         )
+        //     })) : (
+        //         <h4> Is loading ...</h4>
+        //     ) 
+        // }
+        // </div>
 
-          {/* (users.filter(users => users.fan_id === ${userId.fan_id} =>  */}
+
+        // <div className="Fan-Dashboard">
+        //     {renderOnline}
+        // </div>
+
+        <div className="Fan-Dashboard">
+            {fan.length > 0  ? 
+                (fan.map(fan => 
+                    { 
+                return(
+                    
+                    <DashboardCard key={fan.fan_id} fan={fan} />
+                )
+            })) : (
+                <h4> Is loading ...</h4>
+            ) 
+        }
         </div>
     )
 }
@@ -59,8 +71,8 @@ const Dashboard = (props) => {
 const mapStateToProps = state => {
     return {
         fan: state.fan,
-        loggedIn: state.isLoggedIn
-    }
-}
+        isLoggedIn: state.isLoggedIn
+    };
+};
 
-export default connect(mapStateToProps, { fetchFan })(Dashboard);
+export default connect(mapStateToProps, {})(Dashboard)
