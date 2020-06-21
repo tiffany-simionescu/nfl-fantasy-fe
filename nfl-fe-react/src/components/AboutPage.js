2177a54d72d693af9536f746564ca740a601a86d
Laura <theimer.laura@gmail.com>
2020-06-21 14:44:10 -0400

    have aboutpage complete.

--- nfl-fe-react/src/components/AboutPage.js
+++ nfl-fe-react/src/components/AboutPage.js
@@ -3,64 +3,67 @@ import styled from "styled-components";
 
 import github from "../styling/icomoon/PNG/github.png";
 import linkedin from "../styling/icomoon/PNG/linkedin.png"; 
-
+import LargeDivder from "../styling/LargeDivder.js";  
 import JimPicture from "../styling/images/JimPicture.jpg"; 
 import ThatcherPicture from "../styling/images/ThatcherPicture.png";
 import TiffanyPicture from "../styling/images/TiffanyPicture.jpeg"; 
 import LauraPicture from "../styling/images/LauraPicture.png"; 
 import MockPicture from "../styling/images/MockPicture.png"; 
+import ZorigPicture from "../styling/images/ZorigPicture.jpg"; 
 
 const AboutPage = () => {
     return (
         <div className="AboutPage">
             <Heading className="Title">
             <h1> About Us </h1> 
+            <LargeDivder />
                 </Heading>
             <MainArea className="Card-Area">
                 <TeamMembers className="team-members">
                     <div className="Data-Science">
-                        <Titles> Data Science </Titles> 
+                        <Titles> Data Science  <LargeDivder /> </Titles> 
                     <Box className="Enkihzorig-Batsaikhan">
-                        <img src={MockPicture} alt="Enkhzorig Batsaikhan" height="254px" width="250" />
+                        <img src={ZorigPicture} alt="Enkhzorig Batsaikhan" height="254px" width="200px" />
                         <h3> Enkhzorig Batsaikhan </h3>
                         <a href="https://github.com/ezorigo">
-                            <img src={github} alt="github link" /> </a>
-                        <img src={linkedin} alt="linkedin link" />
+                            <Img src={github} alt="github link" /> </a>
+                        <a href="https://www.linkedin.com/in/   enkhzorig-batsaikhan-b5785a133/">
+                            <Img src={linkedin} alt="linkedin link" /> </a>
                     </Box>
                     <Box className="Jim-King">
                         <img src={JimPicture} alt="Jim King" />
                         <h3>Jim King </h3>
                         <a href="https://github.com/JimKing100" >
-                            <img src={github} alt="github link" /> </a>
+                            <Img src={github} alt="github link" /> </a>
                         <a href="https://www.linkedin.com/in/jimkinghomes/">
-                            <img src={linkedin} alt="linkedin link" /> </a>
+                            <Img src={linkedin} alt="linkedin link" /> </a>
                     </Box>
                     </div>
                 <div className="Web-Development">
-                    <Titles> Web Development </Titles>
+                    <Titles> Web Development  <LargeDivder /> </Titles>
                     <Box className="Thatcher-Phan">
                         <img src={ThatcherPicture} alt="Thatcher Phan" height="254px" width="250px"/>
                         <h3> Thatcher Phan </h3>
                         <a href="https://github.com/Th5tch3r">
-                            <img src={github} alt="github link" /> </a>
+                            <Img src={github} alt="github link" /> </a>
                         <a href="https://www.linkedin.com/in/nghi-phan-6b742b18a/ ">
-                            <img src={linkedin} alt="linkedin link" /> </a>
+                            <Img src={linkedin} alt="linkedin link" /> </a>
                     </Box>
                     <Box className="Tiffany-Simionescu">
                         <img src={TiffanyPicture} alt="Tiffany Simionescu" height="254px" width="250px" />
                         <h3> Tiffany Simionescu </h3>
                         <a href="https://github.com/tiffany-simionescu" >
-                            <img src={github} alt="github link" /> </a>
+                            <Img src={github} alt="github link" /> </a>
                         <a href="https://www.linkedin.com/in/tiffanysimionescu/">
-                            <img src={linkedin} alt="linkedin link"/> </a>
+                            <Img src={linkedin} alt="linkedin link"/> </a>
                     </Box>
                     <Box className="Laura-Theimer">
                         <img src={LauraPicture} alt="Laura Theimer" height="254px" width="250px"/>
                         <h3> Laura Theimer  </h3>
                         <a href="https://github.com/lauralyeinc">
-                            <img src={github} alt="github link" /> </a>
+                            <Img src={github} alt="github link" /> </a>
                         <a href="https://www.linkedin.com/in/theimerlaura/" >
-                            <img src={linkedin} alt="linkedin link" /> </a>
+                            <Img src={linkedin} alt="linkedin link" /> </a>
                     </Box>
                 </div>
                 </TeamMembers>
@@ -72,30 +75,28 @@ const AboutPage = () => {
 export default AboutPage; 
 
 
-const Div = styled.div`
-border: 2px solid red; 
-`
-
 const Box = styled.div`
 box-shadow: 10px 10px 5px grey; 
 border: 1px solid grey; 
-padding: 1rem; 
+padding: 2rem; 
+margin-left: 2rem;
+margin-right: 2rem;
+margin-bottom: 2rem;
 `
 
 const MainArea = styled.div`
 width: 65%;
 margin: auto; 
-padding-bottom: 2rem; 
+padding-bottom: 1rem; 
 `
 
 const Titles = styled.div`
-padding: 2.5rem; 
-font-size: 2rem; 
+padding: 1.5rem; 
+font-size: 2rem;  
 `
 
 const Heading = styled.div`
 background: #ffa630;
-font-color: white; 
 `
 
 const TeamMembers = styled.div`
@@ -103,4 +104,10 @@ display: flex;
 justify-content: space-between;
 width: 75%; 
 `
+
+const Img = styled.img`
+padding: .5rem; 
+`
+
+
 
